7275d9a13b64eaec580bcbf6bd497063c2e444a2
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-08-12 21:55:15 +0600

    changed button name

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
         <ul className="menu menu-horizontal px-1">{navOptions}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn">Click</a>
       </div>
     </div>
   );
