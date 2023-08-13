7e0b2c12fa1920e505b201c7cddcf378e16a201a
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-08-13 13:39:47 +0600

    navbar completed with responsiveness

---
+++ src/assets/icons/CloseIcon.jsx
@@ -0,0 +1,21 @@
+const CloseIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        stroke="#807D7E"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2.5"
+        d="m7 7l10 10M7 17L17 7"
+      />
+    </svg>
+  );
+};
+
+export default CloseIcon;
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,53 +1,86 @@
 import CartIcon from "../../../assets/icons/CartIcon";
+import CloseIcon from "../../../assets/icons/CloseIcon";
+import HamburgerIcon from "../../../assets/icons/HamburgerIcon";
 import HeartIcon from "../../../assets/icons/HeartIcon";
 import LoginIcon from "../../../assets/icons/LoginIcon";
 import SearchIcon from "../../../assets/icons/SearchIcon";
 import logo from "../../../assets/images/euphoria-logo.svg";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+  const searchKey = (event) =>{
+    event.preventDefault();
+    console.log(event.target.search.value);
+  }
   const navOptions = (
     <>
-      <li>
+      <li className="hover:text-black">
         <a>Shop</a>
       </li>
-      <li>
+      <li className="hover:text-black">
         <a>Men</a>
       </li>
-      <li>
+      <li className="hover:text-black">
         <a>Women</a>
       </li>
-      <li>
+      <li className="hover:text-black">
         <a>Combos</a>
       </li>
-      <li>
+      <li className="hover:text-black">
         <a>Jaggers</a>
       </li>
     </>
   );
+
+  const navButtons = (
+    <>
+      <div className="p-2 lg:p-3 bg-[#F6F6F6] hover:bg-[#F1F1F1] rounded-lg cursor-pointer">
+        <HeartIcon></HeartIcon>
+      </div>
+      <div className="p-2 lg:p-3 bg-[#F6F6F6] hover:bg-[#F1F1F1] rounded-lg cursor-pointer">
+        <LoginIcon></LoginIcon>
+      </div>
+      <div className="p-2 lg:p-3 bg-[#F6F6F6] hover:bg-[#F1F1F1] rounded-lg cursor-pointer">
+        <CartIcon></CartIcon>
+      </div>
+    </>
+  );
   return (
-    <div className="flex items-center justify-between lg:px-20 lg:py-8 bg-[#FFFFFF] text-black">
-      <div>
-        <img src={logo} alt="logo" />
-      </div>
-      <div>
-        <ul className="flex gap-4 text-base">{navOptions}</ul>
-      </div>
-      <div className="flex justify-between items-center gap-14">
-        <form className="flex items-center flex-row-reverse bg-[#F6F6F6] gap-3 py-2 px-5 rounded-lg">
-          <input className="bg-[#F6F6F6] outline-none" type="text" name="search" />
+    <div className="flex items-center justify-between px-4 py-2 lg:px-20 lg:py-8 bg-[#FFFFFF] text-black">
+      <img className="w-16 lg:w-[90px] mr-4" src={logo} alt="logo" />
+      <ul className="hidden md:flex gap-4 text-gray cursor-pointer text-xs lg:text-sm">
+        {navOptions}
+      </ul>
+      <div className="flex justify-between items-center gap-8">
+        <form onSubmit={searchKey} className="flex items-center flex-row-reverse bg-[#F6F6F6] gap-3 py-2 px-2 lg:px-5 rounded-lg">
+          <input
+            className="bg-[#F6F6F6] w-32 lg:w-48 outline-none"
+            type="text"
+            name="search"
+          />
           <button type="submit">
             <SearchIcon></SearchIcon>
           </button>
         </form>
-        <div className="flex items-center gap-2">
-          <div className="p-3 bg-[#F6F6F6] rounded-lg cursor-pointer">
-            <HeartIcon></HeartIcon>
-          </div>
-          <div className="p-3 bg-[#F6F6F6] rounded-lg cursor-pointer">
-            <LoginIcon></LoginIcon>
-          </div>
-          <div className="p-3 bg-[#F6F6F6] rounded-lg cursor-pointer">
-            <CartIcon></CartIcon>
+        <div className="hidden lg:flex items-center gap-2">{navButtons}</div>
+        <div
+          onClick={() => {
+            setMenu(!menu);
+          }}
+          className="block lg:hidden p-2 bg-[#F6F6F6] hover:bg-[#F1F1F1] rounded-lg cursor-pointer"
+        >
+          {menu ? <CloseIcon></CloseIcon> : <HamburgerIcon></HamburgerIcon>}
+          {/* hamburger expand */}
+          <div
+            className={`${
+              menu ? "fixed" : "hidden"
+            } right-4 top-[60px] bg-[#F6F6F6] p-4 rounded-lg`}
+          >
+            <ul className="flex md:hidden flex-col gap-4 text-gray cursor-pointer text-xs lg:text-sm text-center mb-4">
+              {navOptions}
+            </ul>
+            <div className="flex gap-2">{navButtons}</div>
           </div>
         </div>
       </div>
