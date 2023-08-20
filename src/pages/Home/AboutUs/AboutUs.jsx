203a3bdc3554f2faa12a436592fc172d3b4dfa87
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-08-20 12:42:22 +0600

    done about us section

--- src/pages/Home/AboutUs/AboutUs.jsx
+++ src/pages/Home/AboutUs/AboutUs.jsx
@@ -1,11 +1,38 @@
 import Heading from "../../Shared/Heading/Heading";
-
+import logo from "../../../assets/images/euphoria-logo.svg";
+import GmailIcon from "../../../assets/icons/GmailIcon";
+import CellIcon from "../../../assets/icons/CellIcon";
 const AboutUs = () => {
-    return (
-        <div className="py-10 px-4 lg:px-10">
-            <Heading title={"About Us"}/>
+  return (
+    <div className="py-10 px-4 lg:px-10">
+      <Heading title={"About Us"} />
+      <div className="flex flex-col-reverse lg:flex-row gap-8 py-10 items-center">
+        <div className="w-full lg:w-1/2">
+          <img className="mb-4 w-40" src={logo} alt="logo" />
+          <p className="text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui vel
+            morbi cursus sed sodales molestie proin dictum gravida. Porttitor
+            maecenas tincidunt ipsum semper malesuada. In sapien feugiat laoreet
+            convallis eu sed. Sapien et montes, duis tempor euismod augue cras
+            eu eget. Risus suspendisse mauris ullamcorper felis a, quam. Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Dui vel morbi
+            cursus sed sodales molestie proin dictum gravida. Porttitor maecenas
+            tincidunt ipsum semper malesuada. In sapien feugiat laoreet
+            convallis eu sed. Sapien et montes, duis tempor euismod augue cras
+            eu eget. Risus suspendisse mauris ullamcorper felis a, quam.Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Dui vel morbi
+            cursus sed sodales molestie proin dictum gravida. Porttitor maecenas
+          </p>
+          <p className="mt-4 font-bold mb-4">Contact Information</p>
+          <p className="flex items-center gap-2"><span><CellIcon/></span> +880 1700 000 000</p>
+          <p className="flex items-center gap-2"><span><GmailIcon/></span> xyz@gmail.com</p>
         </div>
-    );
+        <div className="w-full lg:w-1/2">
+            <img className="w-full mt-auto" src="https://i.ibb.co/WynmcrK/shop-pic.png" alt="shop_pic" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
--- src/pages/Home/TopBrandsDeal/TopBrandsDeal.jsx
+++ src/pages/Home/TopBrandsDeal/TopBrandsDeal.jsx
@@ -9,19 +9,19 @@ const TopBrandsDeal = () => {
                 <p className="text-lg font-normal mb-8 lg:mb-14">Up To <span className="font-bold text-[#FBD103]">60%</span> off on brands</p>
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 lg:gap-6">
                     <div className="flex items-center justify-center p-1 lg:p-4 bg-[#ffffff] rounded-lg">
-                        <img src="https://i.ibb.co/KhrNtyJ/h-m.png" alt="" />
+                        <img className="scale-50 md:scale-75 lg:scale-100" src="https://i.ibb.co/KhrNtyJ/h-m.png" alt="" />
                     </div>
                     <div className="flex items-center justify-center p-1 lg:p-4 bg-[#ffffff] rounded-lg">
-                        <img src="https://i.ibb.co/dKk5c6s/puma.png" alt="" />
+                        <img className="scale-50 md:scale-75 lg:scale-100" src="https://i.ibb.co/dKk5c6s/puma.png" alt="" />
                     </div>
                     <div className="flex items-center justify-center p-1 lg:p-4 bg-[#ffffff] rounded-lg">
-                        <img src="https://i.ibb.co/X34P59z/nike.png" alt="" />
+                        <img className="scale-50 md:scale-75 lg:scale-100" src="https://i.ibb.co/X34P59z/nike.png" alt="" />
                     </div>
                     <div className="flex items-center justify-center p-1 lg:p-4 bg-[#ffffff] rounded-lg">
-                        <img src="https://i.ibb.co/VVqx8R2/levi-s.png" alt="" />
+                        <img className="scale-50 md:scale-75 lg:scale-100" src="https://i.ibb.co/VVqx8R2/levi-s.png" alt="" />
                     </div>
                     <div className="flex items-center justify-center p-1 lg:p-4 bg-[#ffffff] rounded-lg">
-                        <img src="https://i.ibb.co/tpFFNLz/us-polo.png" alt="" />
+                        <img className="scale-50 md:scale-75 lg:scale-100" src="https://i.ibb.co/tpFFNLz/us-polo.png" alt="" />
                     </div>
                 </div>
             </div>
